807475800db760c7cc10cbb5aab6befc29bb342a
Yonghye Kwon <developer.0hye@gmail.com>
2021-05-11 21:39:55 +0900

    Update dcn.py

--- dcn.py
+++ dcn.py
@@ -44,9 +44,9 @@ class DeformableConv2d(nn.Module):
     
     def forward(self, x):
         h, w = x.shape[2:]
-        max_offset = max(h, w)/4.
+        #max_offset = max(h, w)/4.
 
-        offset = self.offset_conv(x).clamp(-max_offset, max_offset)
+        offset = self.offset_conv(x)#.clamp(-max_offset, max_offset)
         modulator = 2. * torch.sigmoid(self.modulator_conv(x))
         
         x = torchvision.ops.deform_conv2d(input=x, 
